0614d46be3e1a1125b9c15755c0659338dd84db0
Ilyas Mohamed <zswaleh@ymail.com>
2023-03-24 08:27:31 -0500

    redid part 1

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,31 +1,21 @@
 console.log('***** Music Collection *****')
-//** 1st part
+//* Part 1
+//- Create a variable `collection` that starts as an empty array.
 let Collection = [];
-// Create a variable `collection` that starts as an empty array.
-
+//Add a function named `addToCollection`. This function should:
+//- Take in the album's `title`, `artist`, `yearPublished` as input parameters
 function addToCollection(title, artist, yearPublished) {
-    console.log('In addToCollection',title, artist, yearPublished );
     return Collection[Collection.length -1];
 }
-
-
-//Add a function named `addToCollection`. This function should:
-//  - Take in the album's `title`, `artist`, `yearPublished` as input parameters
-
-
-let music = {
+//Create a new object having the above properties
+//- Add the new object to the end of the `collection` array
+let album1 = {
     title: 'Mr. Nice',
-    artist: "King'asti",
+    artist: 'King asti',
     yearPublished: 2006,
-}
-//  - Create a new object having the above properties
+  }
+  Collection.push(album1);
+// - Return the newly created object
+console.log(addToCollection(album1));
 
-Collection.push(music);
-//  - Add the new object to the end of the `collection` array
-
-//- Return the newly created object
-console.log(addToCollection());
-
-
-
-
+  
